85981894a687ad7949af6c1c9d663702962ff1e9
Hadi Dbouk <hadi.dbouk@edayna.com>
2020-01-26 16:32:26 +0200

    Added Initialize public function to User and Plan in Domain

--- Domain/Domain/Models/User.swift
+++ Domain/Domain/Models/User.swift
@@ -45,6 +45,75 @@ public struct User: Identifiable {
   public let collaborators: Int
   public let twoFactorAuthentication: Bool
   public let plan: Plan
+
+
+  public init(id: Identifier,
+              username: String,
+              nodeId: String,
+              avatarUrl: String,
+              gravatarId: String,
+              url: String,
+              htmlUrl: String,
+              followersUrl: String,
+              followingUrl: String,
+              gistsUrl: String,
+              starredUrl: String,
+              subscriptionsUrl: String,
+              organizationsUrl: String,
+              reposUrl: String,
+              eventsUrl: String,
+              receivedEventsUrl: String,
+              type: String,
+              siteAdmin: Bool,
+              name: String,
+              company: String,
+              blog: String,
+              location: String,
+              email: String,
+              hireable: Bool,
+              bio: String,
+              publicRepos: Int,
+              publicGists: Int,
+              totalPrivateRepos: Int,
+              ownedPrivateRepos: Int,
+              diskUsage: Int,
+              collaborators: Int,
+              twoFactorAuthentication: Bool,
+              plan: Plan) {
+    self.id = id
+    self.username = username
+    self.nodeId = nodeId
+    self.avatarUrl = avatarUrl
+    self.gravatarId = gravatarId
+    self.url = url
+    self.htmlUrl = htmlUrl
+    self.followersUrl = followersUrl
+    self.followingUrl = followingUrl
+    self.gistsUrl = gistsUrl
+    self.starredUrl = starredUrl
+    self.subscriptionsUrl = subscriptionsUrl
+    self.organizationsUrl = organizationsUrl
+    self.reposUrl = reposUrl
+    self.eventsUrl = eventsUrl
+    self.receivedEventsUrl = receivedEventsUrl
+    self.type = type
+    self.siteAdmin = siteAdmin
+    self.name = name
+    self.company = company
+    self.blog = blog
+    self.location = location
+    self.email = email
+    self.hireable = hireable
+    self.bio = bio
+    self.publicRepos = publicRepos
+    self.publicGists = publicGists
+    self.totalPrivateRepos = totalPrivateRepos
+    self.ownedPrivateRepos = ownedPrivateRepos
+    self.diskUsage = diskUsage
+    self.collaborators = collaborators
+    self.twoFactorAuthentication = twoFactorAuthentication
+    self.plan = plan
+  }
 }
 
 public extension User {
@@ -53,5 +122,12 @@ public extension User {
     public let space: Int
     public let privateRepos: Int
     public let collaborators: Int
+
+    public init(name: String, space: Int, privateRepos: Int, collaborators: Int) {
+      self.name = name
+      self.space = space
+      self.privateRepos = privateRepos
+      self.collaborators = collaborators
+    }
   }
 }
